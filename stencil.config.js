c5e58a1582f74b4fe91b50f5cc4be6469c026e63
Jag Reehal <jag.reehal@gmail.com>
2018-02-08 15:00:10 +0000

    Updates

--- stencil.config.js
+++
@@ -1,8 +0,0 @@
-exports.config = {
-  namespace: 'stencil-app',
-  generateDistribution: true,
-  generateWWW: true,
-  bundles: [{ components: ['fancy-tabs'] }]
-};
-
-exports.devServer = { root: 'www', watchGlob: '**/**' };
